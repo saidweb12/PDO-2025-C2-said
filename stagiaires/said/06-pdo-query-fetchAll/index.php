6ea68b85e3146096a829b7d53955380a86af7ee4
saidweb12 <said-farah@hotmail.com>
2025-03-12 15:42:59 +0100

    pdo-query

---
+++ stagiaires/said/06-pdo-query-fetchAll/index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+// on appelle le fichier config-prod.php(requis une seule fois)
+require_once "config-prod.php";
+
+// on se connecte a la DB'pdo_c2' via PDO
+try{
+    // instanciation avec PDO
+    $db = new PDO(
+        DB_CONNECT_TYPE.":host=".DB_CONNECT_HOST.";dbname=".DB_CONNECT_NAME.";port=".DB_CONNECT_PORT.";charset=".DB_CONNECT_CHARSET,
+        DB_CONNECT_USER,
+        DB_CONNECT_PWD,
+    );
+    // on va attribuer à la connexion la manière de gérer les données par PHP
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+// si erreur, instanciation de 'Exception' avec $e comme pointeur
+}catch(Exception $e){
+    // arrêt du script avec die(), et affichage de la méthode se trouvant dans l'instance de 'Exception' via $e
+    die($e->getMessage());
+}
+
