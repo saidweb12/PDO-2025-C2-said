850a72b2306c5af4785b5230ff7ed67ec2330d57
WebDevCF2m <gitweb@cf2m.be>
2025-03-19 16:14:25 +0100

    07

---
+++ stagiaires/reda/07-3-tables/config-dev.php
@@ -0,0 +1,9 @@
+<?php
+# fichier de configuration de PDO en mode développement
+const DB_CONNECT_TYPE = "mysql"; // MySQL et MariaDB
+const DB_CONNECT_HOST = "localhost";
+const DB_CONNECT_PORT = 3306;
+const DB_CONNECT_NAME = "pdo_c2";
+const DB_CONNECT_CHARSET = "utf8";
+const DB_CONNECT_USER = "root";
+const DB_CONNECT_PWD = "";
