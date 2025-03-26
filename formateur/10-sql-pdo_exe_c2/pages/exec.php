a2fe142906885574f7bdd907998e46a947356806
WebDevCF2m <gitweb@cf2m.be>
2025-03-26 11:19:48 +0100

    exec update

--- formateur/10-sql-pdo_exe_c2/pages/exec.php
+++ formateur/10-sql-pdo_exe_c2/pages/exec.php
@@ -6,13 +6,21 @@ $texte = "Le timestamp est le temps depuis le 01/01/1970 en secondes, ou microse
 
 // insertion dans la DB avec exec
 $insert = $db->exec("
-    INSERT INTO `articles` (`id`, `titre`, `texte`, `date_creation`) VALUES (NULL, '$titre', '$texte', CURRENT_TIMESTAMP)
+    INSERT INTO `articles` (`id`, `titre`, `texte`, `date_creation`) 
+    VALUES (NULL, '$titre', '$texte', CURRENT_TIMESTAMP)
 ");
 
 // $insert contient le nombre de lignes affectées (ici 1)
 
 // on peut récupérer le dernier id inséré par cette connexion (utile pour les multiples insertions)
 $insertArticlesId = $db->lastInsertId();
+
+// update de la table articles
+$updateArticle = $db->exec("
+    UPDATE articles SET date_creation = CURRENT_TIMESTAMP
+    WHERE id = 2 OR id = 3;
+");
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -45,8 +53,27 @@ $insertArticlesId = $db->lastInsertId();
 <?php
 // vues
 echo "<h3>Requête 1 avec exec</h3>";
-echo "<pre>INSERT INTO `articles` (`id`, `titre`, `texte`, `date_creation`) VALUES (NULL, '$titre', '$texte', CURRENT_TIMESTAMP)</pre>";
+echo "<pre>INSERT INTO `articles` (`id`, `titre`, `texte`, `date_creation`) 
+VALUES (NULL, '$titre', 
+'$texte', CURRENT_TIMESTAMP)</pre>";
 echo "Nombre de ligne affecté : $insert, puis id de ce dernier article inséré : $insertArticlesId ";
+echo "<hr>";
+?>
+<p>Voici les lignes utilisées Pour la mise à jour (UPDATE)</p>
+<code><pre>
+// update de la table articles
+$updateArticle = $db->exec("
+    UPDATE articles SET date_creation = CURRENT_TIMESTAMP
+    WHERE id = 2 OR id = 3;
+");
+    </pre></code>
+
+<?php
+// vues
+echo "<h3>Requête 2 avec exec</h3>";
+echo "<pre>UPDATE articles SET date_creation = CURRENT_TIMESTAMP
+    WHERE id = 2 OR id = 3;</pre>";
+echo "Nombre de lignes affectées : $updateArticle";
 echo "<hr>";
 ?>
 </body>
