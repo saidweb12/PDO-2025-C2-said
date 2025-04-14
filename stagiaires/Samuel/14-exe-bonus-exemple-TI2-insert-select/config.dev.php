7609ac9c9098b4aa64f7b37d110d1d5950f17f9c
mikhawa <michaeljpitz@gmail.com>
2025-04-14 15:20:21 +0200

    ok

--- stagiaires/Samuel/14-exe-bonus-exemple-TI2-insert-select/config.dev.php
+++ stagiaires/Samuel/14-exe-bonus-exemple-TI2-insert-select/config.dev.php
@@ -4,7 +4,7 @@
 const DB_CONNECT_TYPE = "mysql"; // MySQL et MariaDB
 const DB_CONNECT_HOST = "localhost";
 const DB_CONNECT_PORT = 3306;
-const DB_CONNECT_NAME = "pdo_c2_prepa_ti2";
+const DB_CONNECT_NAME = "pdo_c2_exe_ti2";
 const DB_CONNECT_CHARSET = "utf8";
 const DB_CONNECT_USER = "root";
 const DB_CONNECT_PWD = "";
